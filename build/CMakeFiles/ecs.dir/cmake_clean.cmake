57d72ca210bafab0d4c42f53ae1d9df5f365bf3b
chaoliuzihan <yyhanlei@gmail.com>
2018-06-21 16:03:30 +0800

    build

---
+++ build/CMakeFiles/ecs.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ecs.dir/function.cpp.o"
+  "CMakeFiles/ecs.dir/ecs.cpp.o"
+  "CMakeFiles/ecs.dir/io.cpp.o"
+  "CMakeFiles/ecs.dir/DeployAlgorithm.cpp.o"
+  "CMakeFiles/ecs.dir/predict.cpp.o"
+  "CMakeFiles/ecs.dir/PredictAlgorithm.cpp.o"
+  "CMakeFiles/ecs.dir/DataCenter.cpp.o"
+  "CMakeFiles/ecs.dir/Tools.cpp.o"
+  "/home/big/sdk-gcc/bin/ecs.pdb"
+  "/home/big/sdk-gcc/bin/ecs"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ecs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
